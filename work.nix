d26c29857f17d2d3b1300e61eef6f6188fc0e318
dadada <dadada@dadada.li>
2020-02-29 19:25:12 +0100

    Enable bash and let it exec fish, since changing login shell might break things

--- work.nix
+++ work.nix
@@ -31,6 +31,13 @@ in
     unzip
   ];
 
+  programs.bash = {
+    enable = true;
+    initExtra = ''
+      exec fish
+    '';
+  };
+
   # This value determines the Home Manager release that your
   # configuration is compatible with. This helps avoid breakage
   # when a new Home Manager release introduces backwards

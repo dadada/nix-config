50c840f0d95290101a0eaf96ae587fc2381b3892
dadada <dadada@dadada.li>
2020-03-10 22:48:27 +0100

    Fix gtk config

--- gtk.nix
+++ gtk.nix
@@ -1,10 +1,16 @@
 { config, pkgs, ... }:
 {
   gtk = {
-    theme.package = pkgs.gnome-themes-extra;
-    theme.name = "Adwaita Dark:";
-    iconTheme.package = pkgs.adwaita-icon-theme;
+    enable = true;
+    theme.package = pkgs.gnome3.gnome-themes-extra;
+    theme.name = "Adwaita Dark";
+    iconTheme.package = pkgs.gnome3.adwaita-icon-theme;
     iconTheme.name = "Adwaita";
     font.package = pkgs.cantarell-fonts;
+    font.name = "Cantarell";
   };
+  dconf.settings."org/gnome/desktop/interface" = {
+    enable-animations = false;
+  };
+  qt.platformTheme = true;
 }

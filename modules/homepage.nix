dcb3a8897c564a87942fe7e18ada341db210b269
dadada <dadada@dadada.li>
2020-12-30 23:55:47 +0100

    Fixup

--- modules/homepage.nix
+++ modules/homepage.nix
@@ -6,7 +6,7 @@ let
   homePage = import (builtins.fetchTarball {
     url = "https://github.com/dadada/dadada.li/archive/cb85ed00594f1d4396fe3f61f92e0ff19595596d.tar.gz";
     sha256 = "176l913xsg9gicglkmpmnqwjn8r0psyyj2vx5pi26v0angnfg80a";
-  });
+  }) {};
 in {
   options.dadada.homePage = {
     enable = mkEnableOption "Enable home page";

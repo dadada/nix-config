cdfa5ed4a557690b0d430cf25c597ea58c4a5b10
dadada <dadada@dadada.li>
2020-12-31 01:51:15 +0100

    Fix package

--- modules/homepage.nix
+++ modules/homepage.nix
@@ -3,10 +3,10 @@
 with lib;
 let
   cfg = config.dadada.homePage;
-  homePage = builtins.fetchTarball {
+  homePage = import (builtins.fetchTarball {
     url = "https://github.com/dadada/dadada.li/archive/c77ffc04882f32c2feced7d0f2d8ce3622060230.tar.gz";
       sha256 = "1b48m13yjmw7bpm1jikydv8janys07l6l37yhs1znnj4ygl4hz1d";
-  };
+  });
 in {
   options.dadada.homePage = {
     enable = mkEnableOption "Enable home page";

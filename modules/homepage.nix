71cca6539f893e1f6a12f2699ba52c7ada2988a8
dadada <dadada@dadada.li>
2020-12-31 01:18:26 +0100

    Fixup hash

--- modules/homepage.nix
+++ modules/homepage.nix
@@ -5,7 +5,7 @@ let
   cfg = config.dadada.homePage;
   homePage = builtins.fetchTarball {
     url = "https://github.com/dadada/dadada.li/archive/c77ffc04882f32c2feced7d0f2d8ce3622060230.tar.gz";
-      sha256 = "0mdqa9w1p6cmli6976v4wi0sw9r4p5prkj7lzfd1877wk11c9c73";
+      sha256 = "1b48m13yjmw7bpm1jikydv8janys07l6l37yhs1znnj4ygl4hz1d";
   };
 in {
   options.dadada.homePage = {

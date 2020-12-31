eb665c9be572e9a32e9f886c23f250997edfccb3
dadada <dadada@dadada.li>
2020-12-31 01:26:20 +0100

    Fixup checksum

--- modules/homepage.nix
+++ modules/homepage.nix
@@ -5,7 +5,7 @@ let
   cfg = config.dadada.homePage;
   homePage = builtins.fetchTarball {
     url = "https://github.com/dadada/dadada.li/archive/c77ffc04882f32c2feced7d0f2d8ce3622060230.tar.gz";
-      sha256 = "18jmwz8d6ap2wv9y6p0pb4w1n8vhm8fmjnpg5ngwl9dmhxq2svfv";
+      sha256 = "1b48m13yjmw7bpm1jikydv8janys07l6l37yhs1znnj4ygl4hz1d";
   };
 in {
   options.dadada.homePage = {

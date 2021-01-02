13ff1792cbf0b8998123eb8c218bdeb2d50109c6
dadada <dadada@dadada.li>
2021-01-02 16:40:16 +0100

    Use EFI bootloader

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # nix configuration
 
 Use at your own risk.
+
+## TODO
+
+- Reverse-proxy Hydra via Wireguard tunnel and nginx
--- hosts/pruflas/default.nix
+++ hosts/pruflas/default.nix
@@ -69,10 +69,11 @@ in
     acceptTerms = true;
   };
 
-  # Use the GRUB 2 boot loader.
-  boot.loader.grub.enable = true;
-  boot.loader.grub.version = 2;
-  boot.loader.grub.device = "/dev/sda";
+  boot.kernelModules = [ "kvm-intel" ];
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
 
   swapDevices = [
     {

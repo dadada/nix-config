42c91509027397a1acc27a4593849dc3706b0d7d
dadada <dadada@dadada.li>
2021-01-03 18:48:43 +0100

    Enable caching for gitea

--- modules/gitea.nix
+++ modules/gitea.nix
@@ -1,5 +1,6 @@
 { config, pkgs, lib, ... }:
 let
+  redisSocket = "127.0.0.1:6379";
   cfg = config.dadada.gitea;
 in
 {
@@ -20,6 +21,36 @@ in
         type = "postgres";
       };
       disableRegistration = true;
+      settings = {
+        server = {
+          LANDING_PAGE = "explore";
+          OFFLINE_MODE = true;
+        };
+        picture = {
+          DISABLE_GRAVATAR = true;
+          REPOSITORY_AVATAR_FALLBACK = "random";
+          ENABLE_FEDERATED_AVATAR = false;
+        };
+        other = {
+          SHOW_FOOTER_BRANDING = false;
+          SHOW_FOOTER_VERSION = false;
+          SHOW_FOOTER_TEMPLATE_LOAD_TIME = false;
+        };
+        log = {
+          DISABLE_ROUTER_LOG = true;
+        };
+        cache = {
+          ENABLE = true;
+          ADAPTER = "redis";
+          HOST = "network=tcp,addr=${redisSocket},db=0,pool_size=100,idle_timeout=180";
+        };
+      };
+    };
+
+    services.redis = {
+      enable = true;
+      vmOverCommit = true;
+      #unixSocket = redisSocket;
     };
 
     services.nginx.virtualHosts."git.${config.networking.domain}" = {

89d331cd951bf2cfc02fd83c97d82f74e4a89bd3
dadada <dadada@dadada.li>
2021-04-20 21:33:17 +0200

    Fix unbearable colors

--- modules/home/alacritty/default.nix
+++ modules/home/alacritty/default.nix
@@ -8,15 +8,16 @@ in
     enable = mkEnableOption "Enable alacritty config";
   };
   config = mkIf cfg.enable {
-    home.packages = with pkgs; [
-      jetbrains-mono
+    fonts.fontconfig.enable = true;
+    home.packages = [
+      pkgs.source-code-pro
     ];
     programs.alacritty = {
       enable = true;
       settings = {
         scrolling.history = 0;
         font = {
-          size = 8;
+          size = 10;
           normal = {
             family = "Source Code Pro";
             style = "Regular";
@@ -36,87 +37,54 @@ in
         };
         shell.program = "tmux";
         window.decorations = "none";
-        # XTerm's default colors
         colors = {
+          # Base16 Spacemacs 256 - alacritty color config
+          # Nasser Alshammari (https://github.com/nashamri/spacemacs-theme)
+          # Default colors
           primary = {
-            background = "#ffffff";
-            foreground = "#000000";
+            background = "0x1f2022";
+            foreground = "0xa3a3a3";
           };
+
           # Colors the cursor will use if `custom_cursor_colors` is true
           cursor = {
-            text = "#ffffff";
-            cursor = "#000000";
+            text = "0x1f2022";
+            cursor = "0xa3a3a3";
           };
+
+          # Normal colors
           normal = {
-            black = "#000000";
-            red = "#cd0000";
-            green = "#00cd00";
-            yellow = "#cdcd00";
-            blue = "#0000ee";
-            magenta = "#cd00cd";
-            cyan = "#00cdcd";
-            white = "#e5e5e5";
+            black = "0x1f2022";
+            red = "0xf2241f";
+            green = "0x67b11d";
+            yellow = "0xb1951d";
+            blue = "0x4f97d7";
+            magenta = "0xa31db1";
+            cyan = "0x2d9574";
+            white = "0xa3a3a3";
           };
 
+          # Bright colors
           bright = {
-            black = "#7f7f7f";
-            red = "#ff0000";
-            green = "#00ff00";
-            yellow = "#ffff00";
-            blue = "#5c5cff";
-            magenta = "#ff00ff";
-            cyan = "#00ffff";
-            white = "#ffffff";
+            black = "0x585858";
+            red = "0xf2241f";
+            green = "0x67b11d";
+            yellow = "0xb1951d";
+            blue = "0x4f97d7";
+            magenta = "0xa31db1";
+            cyan = "0x2d9574";
+            white = "0xf8f8f8";
           };
+
+          indexed_colors = [
+            { index = 16; color = "0xffa500"; }
+            { index = 17; color = "0xb03060"; }
+            { index = 18; color = "0x282828"; }
+            { index = 19; color = "0x444155"; }
+            { index = 20; color = "0xb8b8b8"; }
+            { index = 21; color = "0xe8e8e8"; }
+          ];
         };
-        #colors = {
-        #  # Base16 Spacemacs 256 - alacritty color config
-        #  # Nasser Alshammari (https://github.com/nashamri/spacemacs-theme)
-        #  # Default colors
-        #  primary = {
-        #    background = "0x1f2022";
-        #    foreground = "0xa3a3a3";
-        #  };
-
-        #  # Colors the cursor will use if `custom_cursor_colors` is true
-        #  cursor = {
-        #    text = "0x1f2022";
-        #    cursor = "0xa3a3a3";
-        #  };
-
-        #  # Normal colors
-        #  normal = {
-        #    black = "0x1f2022";
-        #    red = "0xf2241f";
-        #    green = "0x67b11d";
-        #    yellow = "0xb1951d";
-        #    blue = "0x4f97d7";
-        #    magenta = "0xa31db1";
-        #    cyan = "0x2d9574";
-        #    white = "0xa3a3a3";
-        #  };
-
-        #  # Bright colors
-        #  bright = {
-        #    black = "0x585858";
-        #    red = "0xf2241f";
-        #    green = "0x67b11d";
-        #    yellow = "0xb1951d";
-        #    blue = "0x4f97d7";
-        #    magenta = "0xa31db1";
-        #    cyan = "0x2d9574";
-        #    white = "0xf8f8f8";
-        #  };
-
-        #  indexed_colors = [
-        #    { index = 16; color = "0xffa500"; }
-        #    { index = 17; color = "0xb03060"; }
-        #    { index = 18; color = "0x282828"; }
-        #    { index = 19; color = "0x444155"; }
-        #    { index = 20; color = "0xb8b8b8"; }
-        #    { index = 21; color = "0xe8e8e8"; }
-        #  ];
-        #};
       };
     };
   };

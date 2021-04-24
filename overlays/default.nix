8b198550cb3aa27ae4e4a230b26573b1a0ab9e2a
dadada <dadada@dadada.li>
2021-04-24 14:30:33 +0200

    Remove sudo overlay

--- overlays/default.nix
+++ overlays/default.nix
@@ -9,7 +9,6 @@
   dadadaScripts = self: super: {
     dadadaScripts = super.callPackage ../pkgs/scripts.nix { };
   };
-  sudo = import ./sudo.nix;
   python3Packages = import ./python3-packages.nix;
   recipemd = self: super: {
     recipemd = super.python3Packages.toPythonApplication super.python3Packages.recipemd;

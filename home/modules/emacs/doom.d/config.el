9822045da52bb0fc94085306171538e57df6ed39
dadada <dadada@dadada.li>
2021-07-30 22:08:14 +0200

    WIP

--- home/modules/emacs/doom.d/config.el
+++ home/modules/emacs/doom.d/config.el
@@ -2,3 +2,8 @@
 (setq org-directory "~/src/notes/org/")
 (with-eval-after-load 'treemacs
   (define-key treemacs-mode-map [mouse-1] #'treemacs-single-click-expand-action))
+(use-package! tree-sitter
+  :config
+  (require 'tree-sitter-langs)
+  (global-tree-sitter-mode)
+  (add-hook 'tree-sitter-after-on-hook #'tree-sitter-hl-mode))

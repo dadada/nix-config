75d78bfb84fac5ed2da91ee6a22e493f2798d912
dadada <dadada@dadada.li>
2021-08-05 22:32:46 +0200

    Revert "remove nix community cache"
    This reverts commit df7004d876fbf5646aa0788343f7f3a5a986fdbc.

--- nixos/configurations.nix
+++ nixos/configurations.nix
@@ -17,10 +17,12 @@ let adapterModule = {
   };
   nix.binaryCaches = [
     https://cache.nixos.org/
+    https://nix-community.cachix.org/
   ];
   nix.binaryCachePublicKeys = [
     "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
     "gorgon:eEE/PToceRh34UnnoFENERhk89dGw5yXOpJ2CUbfL/Q="
+    "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
   ];
   nix.requireSignedBinaryCaches = true;
   nix.useSandbox = true;

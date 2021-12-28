4ddab0af862bc8efdbf53e8576c7c59a25abe8f7
dadada <dadada@dadada.li>
2021-12-28 23:32:07 +0100

    Changed opening of tmux windows and panes to open previous directory

--- home/modules/tmux.nix
+++ home/modules/tmux.nix
@@ -20,6 +20,9 @@ in
         set-option -g status-interval 5
         set-option -g automatic-rename on
         set-option -g automatic-rename-format '#{b:pane_current_path}'
+        bind '"' split-window -c "#{pane_current_path}"
+        bind % split-window -h -c "#{pane_current_path}"
+        bind c new-window -c "#{pane_current_path}"
       '';
     };
   };

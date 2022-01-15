e1239fa84d02e8e552e103342428d66ac6604197
dadada <dadada@dadada.li>
2022-01-15 18:35:35 +0100

    Disabled vi mode in zsh

--- home/modules/zsh.nix
+++ home/modules/zsh.nix
@@ -32,7 +32,6 @@ in
         source ${pkgs.fzf}/share/fzf/key-bindings.zsh
         source ${pkgs.fzf}/share/fzf/completion.zsh
 
-        bindkey -v
         bindkey '^n' autosuggest-accept
 
         preexec() { echo -n -e "\033]0;$1\007" }

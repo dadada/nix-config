1c3918a3c370aa530f7271edbecce3cb8ba111d2
dadada <dadada@dadada.li>
2022-06-28 21:40:40 +0200

    override kanban

--- overlays/default.nix
+++ overlays/default.nix
@@ -2,4 +2,14 @@ let
   python3Packages = import ./python3-packages.nix;
 in
 {
+  kanboard = final: prev: {
+    kanboard = prev.kanboard.overrideAttrs (oldAttrs: {
+      src = prev.fetchFromGitHub {
+        owner = "kanboard";
+        repo = "kanboard";
+        rev = "v${oldAttrs.version}";
+        sha256 = "sha256-WG2lTPpRG9KQpRdb+cS7CqF4ZDV7JZ8XtNqAI6eVzm0=";
+      };
+    });
+  };
 }

c0202bbdef1febcaaba5a2da9f62b00041c1436d
dadada <dadada@dadada.li>
2022-07-09 13:41:31 +0200

    pkgs: add some commandline tools

--- home/home/pkgs.nix
+++ home/home/pkgs.nix
@@ -1,57 +1,84 @@
 { pkgs }:
 with pkgs; [
   anki
+  aqbanking
+  aria2
   aspell
   aspellDicts.de
   aspellDicts.en
   aspellDicts.en-computers
   aspellDicts.en-science
-  aqbanking
   bash
-  bluez-tools
+  bat # cat with syntax highlighting and git integration
   binutils
-  #clang
-  #clang-tools
-  php74Packages.composer
+  bluez-tools
+  btop # htop
+  choose # alternative to cut and awk with more readable syntax
+  colordiff
   darcs
+  delta # feature-rich diff viewer
+  dig
   direnv
-  element-desktop
+  dstat
+  duf # disk usage
+  dyff # diff tool for YAML
   evince
+  exa
   ffmpeg
   file
   firefox
   fractal
+  fx # themable json viewer
+  fzf
   fzf
   gdb
-  gimp
   ghidra-bin
+  gimp
   glow
+  glow # render markdown
   gnome.gnome-tweaks
   gnucash
   gnumake
   gnupg
+  gping # ping with graphs
   graphviz
   grim
+  gron # make json grepable
+  hexyl # hex viewer
+  htop
+  http-prompt
+  httpie
+  hub
+  hyperfine # A command-line benchmarking tool.
+  icdiff
   imagemagick
   inkscape
   inotify-tools
+  ioping # ping but for block devices
+  iproute2
+  iputils # tracepath
   irssi
   jameica
+  jc # convert output to json
   jetbrains.phpstorm
   josm
-  jupyter
   jq
+  jq
+  jupyter
   kcachegrind
   keepassxc
-  #keys
+  kubetail
   ldns
+  liboping # oping, ping multiple hosts at once
   libreoffice
   libvirt
   lsof
+  lynis
   man-pages
   mblaze
   mkpasswd
   mpv
+  mtr
   mumble
   ncurses
   newsflash
@@ -59,26 +86,35 @@ with pkgs; [
   niv
   nix-index
   nmap
+  nmon
   nvd
   obs-studio
   openscad
   openssl
   p7zip
+  pandoc # document converter and templater
   pass
   pavucontrol
+  php74Packages.composer
   pinentry-gnome
   playerctl
+  procs # ps in rust
   prusa-slicer
+  pv
   pwgen
   python3
   python38Packages.dateutil
   python38Packages.managesieve
+  ranger
+  reptyr
+  ripgrep
   ripgrep
   rust-analyzer
   rustup
-  #shortwave
+  sd # search and displace like sed but with better syntax
   signal-desktop
   silver-searcher
+  skim # fzf in Rust
   slurp
   spotify
   sqlite
@@ -87,13 +123,21 @@ with pkgs; [
   tcpdump
   tdesktop
   thunderbird
+  tmux
+  ttyd
   unzip
+  up # ultimate-plumber, interactive pipes
   usbutils
   virt-manager
+  viu # view images from the terminal
   vscodium
   whois
   wireshark
   xdg_utils
+  xmlstarlet
+  xsv # cut for csv
+  xxh # portable shells
   youtube-dl
   zotero
+  zsh
 ]

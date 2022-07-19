9b01699b0ca1de25297a31c27fee8e029d16d388
dadada <dadada@dadada.li>
2022-07-19 21:21:37 +0200

    add app nixos-switch

--- outputs.nix
+++ outputs.nix
@@ -17,6 +17,14 @@
     selfPkgs = self.packages.${system};
   in
   {
+    apps.nixos-switch = {
+      type = "app";
+      program = toString (pkgs.writeScript "deploy" ''
+        #!${pkgs.runtimeShell}
+        flake=$(nix flake metadata --json ${./.} | jq -r .url)
+        nixos-rebuild switch --flake ".#$1" --use-remote-sudo
+      '');
+    };
     apps.deploy = {
       type = "app";
       program = toString (pkgs.writeScript "deploy" ''

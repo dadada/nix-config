bf33bb336524ad6e25b9e02d5a93118d7814f4b5
dadada <dadada@dadada.li>
2022-08-15 19:50:56 +0200

    fix typo

--- nixos/modules/profiles/backup.nix
+++ nixos/modules/profiles/backup.nix
@@ -7,5 +7,5 @@
   };
 
   age.secrets."${config.networking.hostName}-backup-passphrase".file = "${toString secretsPath}/${config.networking.hostName}-backup-passphrase.age";
-  age.secrets."${config.networking.hostName}-backup-ssh-key".file = "${toString secretsPath}/${config.networking.hostName}n-backup-ssh-key.age";
+  age.secrets."${config.networking.hostName}-backup-ssh-key".file = "${toString secretsPath}/${config.networking.hostName}-backup-ssh-key.age";
 }

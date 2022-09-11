fdd2030235f369a19de0ac92e3437148a3c724c3
dadada <dadada@dadada.li>
2022-09-11 14:29:18 +0200

    add update script

--- apps.nix
+++ apps.nix
@@ -6,19 +6,27 @@
 {
   nixos-switch = {
     type = "app";
-    program = toString (pkgs.writeScript "deploy" ''
+    program = toString (pkgs.writeScript "nixos-switch" ''
       #!${pkgs.runtimeShell}
       flake=$(nix flake metadata --json ${./.} | jq -r .url)
-      nixos-rebuild switch --flake ".#$1" --use-remote-sudo
+      ${pkgs.nixos-rebuild}/bin/nixos-rebuild switch --flake ".#$1" --use-remote-sudo
     '');
   };
 
   deploy = {
     type = "app";
-    program = toString (pkgs.writeScript "self-deploy" ''
+    program = toString (pkgs.writeScript "deploy" ''
       #!${pkgs.runtimeShell}
       flake=$(nix flake metadata --json ${./.} | jq -r .url)
       ${deploy-rs.apps."${system}".deploy-rs.program} ''${flake}
+    '');
+  };
+
+  update = {
+    type = "app";
+    program = toString (pkgs.writeScript "update" ''
+      #!${pkgs.runtimeShell}
+      ${pkgs.nix}/bin/nix flake update --commit-lock-file
     '');
   };
 }

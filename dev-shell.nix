f0ab5d3d97b2e49c98fe5c1a70a4f573d09f6250
dadada <dadada@dadada.li>
2022-09-12 20:58:37 +0200

    add deployment scripts to dev-shell

--- dev-shell.nix
+++ dev-shell.nix
@@ -1,7 +1,11 @@
-{ pkgs, agenix, deploy-rs, system, ... }:
+{ self, pkgs, agenix, deploy-rs, system, ... }:
+let
+  selfApp = app: self.apps."${app}";
+in
 pkgs.mkShell {
-  buildInputs = [
-    agenix.defaultPackage."${system}"
-    deploy-rs.defaultPackage."${system}"
+  buildInputs = pkgs.lib.catAttrs "system" [
+    agenix.defaultPackage
+    deploy-rs.defaultPackage
+    (pkgs.lib.getAttrs [ "deploy" "update" "nixos-switch" ] self.apps)
   ];
 }
--- home/modules/gtk.nix
+++ home/modules/gtk.nix
@@ -13,7 +13,7 @@ in
   config = mkIf cfg.enable {
     gtk = {
       enable = true;
-      theme.name = "Adwaita";
+      theme.name = "Adwaita-dark";
     };
   };
 }

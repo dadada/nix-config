e69ffedc067de43b146b94d68011da5d0059a6a0
dadada <dadada@dadada.li>
2022-09-18 18:08:33 +0200

    fixup devshell

--- dev-shell.nix
+++ dev-shell.nix
@@ -3,7 +3,7 @@ let
   selfApp = app: self.apps."${app}";
 in
 pkgs.mkShell {
-  buildInputs = pkgs.lib.catAttrs "system" [
+  buildInputs = pkgs.lib.catAttrs system [
     agenix.defaultPackage
     deploy-rs.defaultPackage
     (pkgs.lib.getAttrs [ "deploy" "update" "nixos-switch" ] self.apps)

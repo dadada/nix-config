e3da8eabd7cbf7b130684f4da2189647e8c22e07
dadada <dadada@dadada.li>
2023-06-09 19:31:42 +0200

    remove global additional binary caches

--- nixos/modules/profiles/base.nix
+++ nixos/modules/profiles/base.nix
@@ -16,17 +16,11 @@ in
   nix.registry = lib.mapAttrs' (name: value: lib.nameValuePair name { flake = value; }) inputs;
   nix.settings.flake-registry = "${config.dadada.inputs.flake-registry}/flake-registry.json";
 
-  nix.settings.substituters = [
-    https://helix.cachix.org/
-    https://cache.nixos.org/
-    https://nix-community.cachix.org/
-  ];
+  nix.settings.substituters = [ https://cache.nixos.org/ ];
 
   nix.settings.trusted-public-keys = [
-    "helix.cachix.org-1:ejp9KQpR1FBI2onstMQ34yogDm4OgU2ru6lIwPvuCVs="
     "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
     "gorgon:eEE/PToceRh34UnnoFENERhk89dGw5yXOpJ2CUbfL/Q="
-    "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
   ];
 
   nix.settings.require-sigs = true;

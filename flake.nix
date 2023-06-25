e53a688e5139d7a0068f828a8d851b51d4cbe928
dadada <dadada@dadada.li>
2023-06-25 22:15:30 +0200

    Upgrade flake.nix

--- flake.nix
+++ flake.nix
@@ -2,25 +2,25 @@
   description = "dadada's nix flake";
 
   inputs = {
-    nixpkgs.url = github:NixOS/nixpkgs/nixos-23.05;
-    flake-utils.url = github:numtide/flake-utils;
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+    flake-utils.url = "github:numtide/flake-utils";
     home-manager = {
-      url = github:nix-community/home-manager/release-23.05;
+      url = "github:nix-community/home-manager/release-23.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    nixos-hardware.url = github:NixOS/nixos-hardware/master;
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
     homePage = {
-      url = github:dadada/dadada.li;
+      url = "github:dadada/dadada.li";
     };
     recipemd = {
-      url = github:dadada/recipemd/nix-flake;
+      url = "github:dadada/recipemd/nix-flake";
     };
     agenix = {
-      url = github:ryantm/agenix/0.13.0;
+      url = "github:ryantm/agenix/0.13.0";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     devshell = {
-      url = github:numtide/devshell;
+      url = "github:numtide/devshell";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nixos-generators = {
@@ -31,7 +31,7 @@
       url = "github:NixOS/flake-registry";
       flake = false;
     };
-    helix.url = "github:helix-editor/helix/23.03";
+    helix.url = "github:helix-editor/helix/23.05";
   };
 
   outputs = { ... } @ args: import ./outputs.nix args;

1ab59d7994f4514c02a51cf1794e9c9f8b0907ca
dadada <dadada@dadada.li>
2023-11-12 22:50:19 +0100

    soft-serve: update

--- pkgs/soft-serve.nix
+++ pkgs/soft-serve.nix
@@ -4,16 +4,16 @@
 
 buildGoModule rec {
   pname = "soft-serve";
-  version = "0.5.4";
+  version = "0.7.1";
 
   src = fetchFromGitHub {
     owner = "charmbracelet";
     repo = "soft-serve";
     rev = "v${version}";
-    sha256 = "sha256-pVUkmia6W5CVYLjrE6Ie2OVme3y2pmhMMxCYS5qyhgs=";
+    sha256 = "sha256-PY/BHfuDRHXpzyUawzZhDr1m0c1tWqawW7GP9muhYAs=";
   };
 
-  vendorHash = "sha256-wf2Dfo4uWHg/h2+EfEW5oGUgqf1kAgiTq7ivczI1w+c=";
+  vendorHash = "sha256-jtEiikjEOThTSrd+UIEInxQmt2z5YVyksuTC17VmdkA=";
 
   doCheck = false;
 

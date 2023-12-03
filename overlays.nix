c7e697284e0c63a918355de7b6304fcff0fb0477
dadada <dadada@dadada.li>
2023-12-03 09:16:56 +0100

    Remove tubslatex overlay
    Use tubslatex-nix flake instead

--- overlays.nix
+++ overlays.nix
@@ -1,33 +1,4 @@
 {
-  tubslatex = final: prev: {
-    # Based on https://gist.github.com/clefru/9ed1186bf0b76d27e0ad20cbd9966b87
-    tubslatex =
-      prev.lib.overrideDerivation
-        (prev.texlive.combine {
-          inherit (prev.texlive) scheme-full;
-          tubslatex.pkgs = [ (prev.callPackage ../pkgs/tubslatex { }) ];
-        })
-        (oldAttrs: {
-          postBuild =
-            ''
-              # Save the udpmap.cfg because texlive.combine removes it.
-              cat $out/share/texmf/web2c/updmap.cfg > $out/share/texmf/web2c/updmap.cfg.1
-            ''
-            + oldAttrs.postBuild
-            + ''
-              # Move updmap.cfg into its original place and rerun mktexlsr, so that kpsewhich finds it
-              rm $out/share/texmf/web2c/updmap.cfg || true
-              cat $out/share/texmf/web2c/updmap.cfg.1 > $out/share/texmf/web2c/updmap.cfg
-              rm $out/share/texmf/web2c/updmap.cfg.1
-              perl `type -P mktexlsr.pl` $out/share/texmf
-              yes | perl `type -P updmap.pl` --sys --syncwithtrees --force || true
-              perl `type -P updmap.pl` --sys --enable Map=NexusProSerif.map --enable Map=NexusProSans.map
-              # Regenerate .map files.
-              perl `type -P updmap.pl` --sys
-            '';
-        });
-  };
-
   kanboard = final: prev: {
     kanboard = prev.kanboard.overrideAttrs (oldAttrs: {
       src = prev.fetchFromGitHub {

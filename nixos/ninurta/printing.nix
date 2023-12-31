78328291dbe28f7c4237c903d93c0c6682c1b349
dadada <dadada@dadada.li>
2023-12-31 17:28:40 +0100

    ninurta: change brother ppl

--- nixos/ninurta/printing.nix
+++ nixos/ninurta/printing.nix
@@ -1,18 +1,11 @@
 { pkgs, ... }:
 {
-  services.printing = {
-    enable = true;
-    drivers = [ pkgs.brlaser ];
-    # Remove all state at the start of the service
-    stateless = true;
-  };
-
   hardware = {
     printers = {
       ensurePrinters = [
         {
-          name = "Brother_HL-L2310D";
-          model = "drv:///brlaser.drv/brl2320d.ppd";
+          name = "Brother_HL-L2300D";
+          model = "drv:///brlaser.drv/brl2300d.ppd";
           location = "BS";
           deviceUri = "usb://Brother/HL-L2310D%20series?serial=E78096H3N771439";
           ppdOptions = {
@@ -35,6 +28,10 @@
   };
 
   services.printing = {
+    enable = true;
+    drivers = [ pkgs.brlaser ];
+    # Remove all state at the start of the service
+    stateless = true;
     listenAddresses = [ "192.168.101.184:631" "fd42:9c3b:f96d:101:4a21:bff:fe3e:9cfe:631" ];
     allowFrom = [ "from 192.168.101.0/24" ];
     browsing = true;
